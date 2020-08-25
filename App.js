74fe8ce49fb19aec980ea4cf471935790bd8769c
abdurrahmanuzmez <abdurrahmanuzmez@outlook.com>
2020-08-25 23:58:54 +0300

    Firebase learning

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+import {createAppContainer, createSwitchNavigator} from "react-navigation";
+import {LoadingScreen} from './src/screens/LoadingScreen'
+import {LoginScreen} from './src/screens/LoginScreen'
+import {DashboardScreen} from './src/screens/DashboardScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export class App extends React.Component  {
+  render() {
+    return <AppNavigator />;
+  }
+
 }
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen: LoadingScreen,
+  LoginScreen: LoginScreen,
+  DashboardScreen: DashboardScreen
+})
+
+const AppNavigator = createAppContainer(AppSwitchNavigator);
 
 const styles = StyleSheet.create({
   container: {
